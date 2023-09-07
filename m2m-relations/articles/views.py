ade84e4a80b10e8187cdf312a648e10d9d13df57
Vitaly <fuhir@yandex.ru>
2023-09-07 23:56:44 +0300

    first commit

---
+++ m2m-relations/articles/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from articles.models import Article
+
+
+def articles_list(request):
+    template = 'articles/news.html'
+    object_list = Article.objects.prefetch_related('scopes').all()
+    context = {'object_list': object_list}
+    return render(request, template, context)
